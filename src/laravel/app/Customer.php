f794de018e02055f101bb1ee603b1867f5a0df57
usuki@go-next.co.jp <f313ed@gmail.com>
2019-05-24 20:49:26 +0900

    phpDocks

--- src/laravel/app/Customer.php
+++ src/laravel/app/Customer.php
@@ -4,7 +4,19 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Customer
+ *
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Report[] $reports
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Customer newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Customer newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Customer query()
+ * @mixin \Eloquent
+ */
 class Customer extends Model
 {
-    //
+    public function reports()
+    {
+        return $this->hasMany(Report::class);
+    }
 }
